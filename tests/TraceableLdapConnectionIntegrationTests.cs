1f2458cd5da5c5370c2ae5ff2f9f5ad8936b03c5
hangy <hangy@hangy.de>
2025-08-24 09:50:54 +0200

    refactor: Move LDAP connection test setup to a base class for better code reuse

--- tests/TraceableLdapConnectionIntegrationTests.cs
+++ tests/TraceableLdapConnectionIntegrationTests.cs
@@ -1,69 +1,11 @@
-using System;
 using System.DirectoryServices.Protocols;
 using System.Net;
-using System.Threading.Tasks;
 using Xunit;
-using DotNet.Testcontainers;
-using DotNet.Testcontainers.Builders;
-using TraceableLdapClient;
-using DotNet.Testcontainers.Containers;
 
 namespace TraceableLdapClient.Tests;
 
-public class TraceableLdapConnectionIntegrationTests : IAsyncLifetime
+public class TraceableLdapConnectionIntegrationTests : TraceableLdapConnectionTestBase
 {
-    private const int LdapPort = 3890;
-    private const string JwtSecret = "REPLACE_WITH_RANDOM";
-    private const string KeySeed = "REPLACE_WITH_RANDOM";
-    private const string LdapBaseDn = "dc=example,dc=com";
-    private const string LdapUserPass = "adminPas$word";
-    private const string AdminUser = "cn=admin,ou=people,dc=example,dc=com";
-    private readonly IContainer _ldapContainer;
-    private string _ldapHost = "localhost";
-    private int _ldapPort;
-
-    public TraceableLdapConnectionIntegrationTests()
-    {
-        _ldapContainer = new ContainerBuilder()
-            .WithImage("docker.io/lldap/lldap:stable")
-            .WithPortBinding(LdapPort, true)
-            .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(LdapPort))
-            .WithEnvironment("LLDAP_JWT_SECRET", JwtSecret)
-            .WithEnvironment("LLDAP_KEY_SEED", KeySeed)
-            .WithEnvironment("LLDAP_LDAP_BASE_DN", LdapBaseDn)
-            .WithEnvironment("LLDAP_LDAP_USER_PASS", LdapUserPass)
-            .Build();
-    }
-
-    public async ValueTask InitializeAsync()
-    {
-        await _ldapContainer.StartAsync().ConfigureAwait(true);
-        _ldapPort = _ldapContainer.GetMappedPublicPort(LdapPort);
-        _ldapHost = _ldapContainer.Hostname;
-    }
-
-    public async ValueTask DisposeAsync()
-    {
-        await _ldapContainer.DisposeAsync().ConfigureAwait(true);
-        GC.SuppressFinalize(this);
-    }
-
-    private TraceableLdapConnection CreateConnection()
-    {
-        var identifier = new LdapDirectoryIdentifier(_ldapHost, _ldapPort);
-        var credential = new NetworkCredential(AdminUser, LdapUserPass);
-        return new TraceableLdapConnection(identifier, credential, AuthType.Basic)
-        {
-            AutoBind = true,
-            Timeout = TimeSpan.FromSeconds(30),
-            SessionOptions =
-            {
-                ReferralChasing = ReferralChasingOptions.All,
-                ProtocolVersion = 3
-            }
-        };
-    }
-
     [Fact]
     public void BindShouldAuthenticateSuccessfully()
     {
