c5f9fee43036d0a05550e18c686846f06166eb35
hangy <hangy@hangy.de>
2025-08-24 09:29:20 +0200

    test: Add wait strategy for LDAP container initialization and update package dependencies

--- tests/TraceableLdapConnectionIntegrationTests.cs
+++ tests/TraceableLdapConnectionIntegrationTests.cs
@@ -27,6 +27,7 @@ public class TraceableLdapConnectionIntegrationTests : IAsyncLifetime
         _ldapContainer = new ContainerBuilder()
             .WithImage("docker.io/lldap/lldap:stable")
             .WithPortBinding(LdapPort, true)
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(LdapPort))
             .WithEnvironment("LLDAP_JWT_SECRET", JwtSecret)
             .WithEnvironment("LLDAP_KEY_SEED", KeySeed)
             .WithEnvironment("LLDAP_LDAP_BASE_DN", LdapBaseDn)
